9d6e58ae7a1c8024fb7fd9d0882f5e1e66c7534e
James Haley <haleyjd@hotmail.com>
2016-11-21 08:17:08 -0600

    Moved gamepad button defines to own header

---
+++ src/jagpad.h
@@ -0,0 +1,59 @@
+/*
+  CALICO
+
+  Jaguar gamepad key constants; separated out of doomdef.h
+*/
+
+#ifndef JAGPAD_H__
+#define JAGPAD_H__
+
+#define JP_NUM    1
+#define JP_9      2
+#define JP_6      4
+#define JP_3      8
+#define JP_0      0x10
+#define JP_8      0x20
+#define JP_5      0x40
+#define JP_2      0x80
+
+#define JP_OPTION 0x200
+#define JP_C      0x2000
+#define JP_STAR   0x10000
+#define JP_7      0x20000
+#define JP_4      0x40000
+#define JP_1      0x80000
+#define JP_UP     0x100000
+#define JP_DOWN   0x200000
+#define JP_LEFT   0x400000
+#define JP_RIGHT  0x800000
+
+#define JP_B      0x2000000
+#define JP_PAUSE  0x10000000
+#define JP_A      0x20000000
+
+#define BT_RIGHT  JP_RIGHT
+#define BT_LEFT   JP_LEFT
+#define BT_UP     JP_UP
+#define BT_DOWN   JP_DOWN
+#define BT_A      JP_A
+#define BT_B      JP_B
+#define BT_C      JP_C
+#define BT_OPTION JP_OPTION
+#define BT_PAUSE  JP_PAUSE
+#define BT_STAR   JP_STAR
+#define BT_HASH   JP_NUM
+#define BT_1      JP_1
+#define BT_2      JP_2
+#define BT_3      JP_3
+#define BT_4      JP_4
+#define BT_5      JP_5
+#define BT_6      JP_6
+#define BT_7      JP_7
+#define BT_8      JP_8
+#define BT_9      JP_9
+#define BT_0      JP_0
+
+#endif
+
+// EOF
+
